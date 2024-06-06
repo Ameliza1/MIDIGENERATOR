5a2f4f18b442d7d309fc41af55addf0cf7532dc4
Elizabeth OWUSU <elizaowusu022@gmail.com>
2024-06-06 16:41:22 +0200

    � NEW: New commit

---
+++ tests/test_chord_generator.py
@@ -0,0 +1,32 @@
+import pytest
+
+from chord_generator import pick_random_chord, calculate_interval
+
+from chord_generator import chords
+
+from chord_generator import evaluate_stability
+
+@pytest.fixture
+def get_chords_Cmin():
+    return [48, 51, 55]
+
+@pytest.fixture
+def get_chords_Emaj():
+    return [52, 56, 59]
+
+
+def test_pick_random_chord():
+    chord = pick_random_chord()
+    assert chord in chords
+
+def test_pick_not_in_chi_chord():
+    not_chord = 'Cmin42'
+    assert not_chord not in chords
+
+def test_calculate_interval(get_chords_Cmin, get_chords_Emaj):
+    interval = calculate_interval(get_chords_Cmin[0], get_chords_Emaj[0])
+    assert interval == 4
+    assert calculate_interval(-8, -9) == 1
+
+def test_evaluate_stability():
+    assert evaluate_stability('Cmin','Emaj') == 100
